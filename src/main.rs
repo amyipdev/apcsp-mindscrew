84cf29404bd8dd9bdf09ff5eb5dd7b0d62b360f6
Amy Parker <apark0006@student.cerritos.edu>
2023-03-03 20:42:24 -0800

    baseline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+const INIMEMSIZE: usize = 64;
+
 fn main() {
-    println!("Hello, world!");
+    // Declare registers, claim memory space
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    let mut z: usize = 0;
+    let mut ip: usize = 0;
+    let mut mem: Vec<usize> = vec![0; INIMEMSIZE];
+
+    // Load program code
+    let ins: Vec<char> = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap().chars().collect();
 }
